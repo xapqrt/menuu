524c0d41abce34d961a47d0a379ab2f9ca456140
Vivaan Pandey <shivikashivikaaa@gmail.com>
2025-12-25 16:16:05 +0530

    first commit, basic file setups!

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+//learning rust for the first time so lets see how it all goess
+
+
+
+use wasm_bindgen::prelude::*;
+
+
+
+//wga an thinking is that i'd js create seperate files so they are easy to organise
+
+
+
+mod menu;
+mod cart;
+mod pricing;
+mod order;
+
+
+
+
+//js
+
+
+pub use menu::*;
+pub use cart::*;
+
+pub use pricing::*;
+pub use order::*;
+
+
+#[wasm_bindgen(start)]
+
+
+pub fn init() {
+
+
+    //uh js gunnga panic hook so it shows errors in browser console
+
+
+
+    #[cfg(feature = "console_error_panic_hook")]
+
+    console_error_panic_hook::set_once();
+
+
+
+    console.log("rustybite initialized");
+
+
+
+}
+
+
+
+#[wasm_bindgen]
+
+extern "c" {
+
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+}
+
+#[macro_export]
+macro_rules! console.log {
+
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+//yup first commit, it was pretty hard took me so many trips to the documentation lmaoo
